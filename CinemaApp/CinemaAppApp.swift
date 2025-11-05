c94aa729cf60597195487db90d4cac9e267788e7
MelnikovAleksandr <melnikovjavadev@gmail.com>
2025-11-04 17:58:51 -0800

    Initial Commit

---
+++ CinemaApp/CinemaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaAppApp.swift
+//  CinemaApp
+//
+//  Created by Александр Мельников on 04.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CinemaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
